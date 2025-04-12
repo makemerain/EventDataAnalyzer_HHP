7124f6a93288ffb58c68934323ee41e4fe5f4b04
makemerain <aralimara@gmail.com>
2025-04-12 12:23:53 +0530

    hanging the event data models to unique, the development till date needs to be changed

---
+++ DataModels/SpeedoMeterModels/MedhaEventDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDataAnalyzer_HHP.DataModels.SpeedoMeterModels
+{
+    internal class MedhaEventDataModel
+    {
+    }
+}
